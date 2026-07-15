b632e294232ef9b554b6911439ea93b8391af38a
charram <charampin2633@gmail.com>
2026-07-15 17:32:48 +0700

    Add API health endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'OpenBeauty API is running',
+        'version' => '1.0.0'
+    ]);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
